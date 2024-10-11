3aa9f92b39ec98abec5f499908a23d79e515c80f
KaguraMio <674126905@qq.com>
2024-10-12 05:37:51 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
     return [{
       source: '/api/:path*',
       //destination: 'https://mock.apifox.cn/m1/2398938-0-default/api/:path*'
-      destination: 'http://localhost:8080/api/:path*'
+      //destination: 'http://localhost:8080/api/:path*'
+      destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
     }]
   }
 }
